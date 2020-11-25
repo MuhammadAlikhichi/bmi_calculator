0dbe2a64553eed6fc2cc7f7bd0f93205f5a3d137
muhammadali <muhammadalikhichi031@gmail.com>
2020-11-25 18:28:00 +0500

    use of refactored code || Lecture 57 || Part 04

--- lib/inputpage.dart
+++ lib/inputpage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -18,9 +19,21 @@ class _InputPageState extends State<InputPage> {
             children: [
               Expanded(child: RepeatContainerCode(
                 colors: Colors.brown,
+                cardwidge: Repeartexticoncode(
+                  iconData: FontAwesomeIcons.male,
+                  Label:' MALE',
+                ),
+
               ),
               ),
-              Expanded(child: RepeatContainerCode(colors: Colors.brown,),
+              Expanded(child: RepeatContainerCode(
+                colors: Colors.brown,
+                cardwidge: Repeartexticoncode(
+                  iconData: FontAwesomeIcons.male,
+                  Label:' FEMALE',
+                ),
+
+              ),
               ),
             ],
           )),
@@ -40,13 +53,44 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class Repeartexticoncode extends StatelessWidget {
+  Repeartexticoncode({@required this.iconData,this.Label});
+  final IconData iconData;
+  final String Label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          iconData,
+          //FontAwesomeIcons.male,
+          size: 85.0,
+        ),
+        SizedBox(height: 15.0),
+        Text(Label,
+          style: TextStyle(
+            fontSize: 30.0,
+            color: Colors.grey,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+
+
 class RepeatContainerCode extends StatelessWidget {
-  RepeatContainerCode({@required this.colors});
-  Color colors;
+  RepeatContainerCode({@required this.colors, this.cardwidge});
+  final Color colors;
+  final Widget cardwidge;
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
+      child: cardwidge,
       decoration: BoxDecoration(
         color: Colors.brown,
         borderRadius: BorderRadius.circular(10.0)
