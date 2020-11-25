c25a73d2813ae55a49f7538e1bb206f9242d377a
muhammadali <muhammadalikhichi031@gmail.com>
2020-11-25 19:19:20 +0500

    use of gesture detector in flutter || Lecture 60

--- lib/inputpage.dart
+++ lib/inputpage.dart
@@ -3,12 +3,29 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'RepeatContainer.dart';
 import 'Repeattexticon.dart';
+const activeColor = Colors.grey;
+const deActiveColor = Colors.brown;
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
 class _InputPageState extends State<InputPage> {
+  Color maleColor =deActiveColor;
+  Color FeMaleColor =deActiveColor;
+  void updateColor(int gender){
+    if(gender==1)
+      {
+        maleColor=activeColor;
+        FeMaleColor=deActiveColor;
+      }
+    if(gender==2)
+      {
+        maleColor=deActiveColor;
+        FeMaleColor=activeColor;
+      }
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,22 +36,36 @@ class _InputPageState extends State<InputPage> {
         children: [
           Expanded(child: Row(
             children: [
-              Expanded(child: RepeatContainerCode(
-                colors: Colors.brown,
-                cardwidge: Repeartexticoncode(
-                  iconData: FontAwesomeIcons.male,
-                  Label:' MALE',
-                ),
+              Expanded(child: GestureDetector(
+                onTap: (){
+                  setState(() {
+                    updateColor(1);
+                  });
+                },
+                child: RepeatContainerCode(
+                  colors: maleColor,
+                  cardwidge: Repeartexticoncode(
+                    iconData: FontAwesomeIcons.male,
+                    Label:' MALE',
+                  ),
 
+                ),
               ),
               ),
-              Expanded(child: RepeatContainerCode(
-                colors: Colors.brown,
-                cardwidge: Repeartexticoncode(
-                  iconData: FontAwesomeIcons.male,
-                  Label:' FEMALE',
-                ),
+              Expanded(child: GestureDetector(
+                onTap: (){
+                  setState(() {
+                    updateColor(2);
+                  });
+                },
+                child: RepeatContainerCode(
+                  colors: FeMaleColor,
+                  cardwidge: Repeartexticoncode(
+                    iconData: FontAwesomeIcons.male,
+                    Label:' FEMALE',
+                  ),
 
+                ),
               ),
               ),
             ],
